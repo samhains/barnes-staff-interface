f53e5bf7f2b8b74f2e923252b28c0f5d6bb9a9b9
Sam Hains <sam@samhains.com>
2017-06-20 16:52:42 +1000

    add styling for home page

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 
-const App = () => (
-  <div>
-    Hello World
+const style = {
+  height: 200,
+  width: 400,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+const containerStyle= {
+  display: 'flex',
+  marginTop: '10%',
+  justifyContent: 'center'
+}
+
+const Home = () => (
+  <div style={containerStyle}>
+
+    <Paper
+        style={style}>
+      <TextField
+        floatingLabelText="Password"
+        type="password"
+      />
+    </Paper>
   </div>
 );
 
-export default App;
+export default Home;
