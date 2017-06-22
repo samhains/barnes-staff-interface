757766a2a300efa0bdec349cdc371608363a1322
Sam Hains <sam@samhains.com>
2017-06-22 15:02:28 +1000

    cleaned up styling

--- src/App.js
+++ src/App.js
@@ -2,45 +2,44 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Link,
 } from 'react-router-dom';
 
-import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import AppBar from 'material-ui/AppBar';
 import Login from './Login';
 import InformationView from './InformationView';
 import ArtistView from './ArtistView';
 import ArtistTagView from './ArtistTagView';
 import TagView from './TagView';
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
 
 import './App.css';
 
 const linkStyle = {
   textDecoration: 'none',
-  color: 'black'
-}
+  color: 'black',
+};
 
-const menuItems = [ 
-	{name: 'Information', url: '/information'},
-	{name: 'Artist', url: '/artist'},
-	{name: 'Artist Tag', url: '/artist-tag'},
-	{name: 'Tag', url: '/tag'},
-
-
-]
+const menuItems = [
+{ name: 'Information', url: '/information' },
+{ name: 'Artist', url: '/artist' },
+{ name: 'Artist Tag', url: '/artist-tag' },
+{ name: 'Tag', url: '/tag' },
+];
 
 const drawerStyle = {
   marginTop: '64px',
-  boxShadow: 'rgba(0, 0, 0, 0.16) 0px 3px 10px, rgba(0, 0, 0, 0.23) 0px 10px 10px'
-}
+  boxShadow: 'rgba(0, 0, 0, 0.16) 0px 3px 10px, rgba(0, 0, 0, 0.23) 0px 10px 10px',
+};
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { open: false };
   }
 
-  handleToggle = () => this.setState({open: !this.state.open});
+  handleToggle() { this.setState({ open: !this.state.open }); }
 
   render() {
     return (
@@ -51,13 +50,19 @@ class App extends Component {
             onLeftIconButtonTouchTap={this.handleToggle}
           >
 
-          <Drawer 
-            containerStyle={drawerStyle}
-            open={this.state.open}>
-            {
-            menuItems.map((item) => ( <Link style={linkStyle} to={item.url}><MenuItem key={item.name}>{item.name}</MenuItem></Link>))
-            }
-          </Drawer>
+            <Drawer
+              containerStyle={drawerStyle}
+              open={this.state.open}
+            >
+              {
+                menuItems.map(
+                  item => (
+                    <Link style={linkStyle} to={item.url}>
+                      <MenuItem key={item.name}>{item.name}</MenuItem>
+                    </Link>),
+                  )
+              }
+            </Drawer>
           </AppBar>
           <Route exact path="/" component={Login} />
           <Route path="/information" component={InformationView} />
