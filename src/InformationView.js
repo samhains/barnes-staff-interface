25ed898d23af1aa4fd227fc010d59dbb97643b99
Sam Hains <sam@samhains.com>
2017-06-23 12:26:36 +1000

    add table

--- src/InformationView.js
+++ src/InformationView.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
 import Wrapper from './Wrapper';
 import Header from './Header';
 import HeaderSmall from './HeaderSmall';
@@ -17,11 +25,17 @@ const innerContainerStyle = {
   flexGrow: '0',
 };
 
+const tagData = [
+  { name: 'Dog', confidence: 0.984 },
+  { name: 'Cat', confidence: 0.984 },
+  { name: 'Mouse', confidence: 0.984 },
+  { name: 'Badger', confidence: 0.984 },
+]
+
 const Main = () => (
   <Wrapper>
     <Header> The Crying Woman by Pablo Picasso </Header>
     <SubHeader> Information about this painting might sit here</SubHeader>
-    
     <div style={containerStyle}>
       <div style={innerContainerStyle}>
         <SearchInput />
@@ -34,6 +48,22 @@ const Main = () => (
           Microsoft Azure
         </HeaderSmall>
         <ServiceSelect />
+        <Table>
+          <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
+            <TableRow>
+              <TableHeaderColumn>Name</TableHeaderColumn>
+              <TableHeaderColumn>Confidence</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+        <TableBody displayRowCheckbox={false}>
+            {tagData.map((tag) => (
+              <TableRow>
+                <TableRowColumn>{tag.name}</TableRowColumn>
+                <TableRowColumn>{tag.confidence}</TableRowColumn>
+              </TableRow>
+            ))}
+        </TableBody>
+        </Table>
       </div>
     </div>
   </Wrapper>
