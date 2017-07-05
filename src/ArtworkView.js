d8735e46e121aa3c8bdfa7fa99fe115a44349121
Sam Hains <sam@samhains.com>
2017-07-05 14:39:07 +1000

    create functional tag view page

---
+++ src/ArtworkView.js
@@ -0,0 +1,152 @@
+import React, { Component } from 'react';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
+
+import { Link } from 'react-router-dom';
+import Wrapper from './Wrapper';
+import Header from './Header';
+import HeaderSmall from './HeaderSmall';
+import SubHeader from './SubHeader';
+
+import SearchInput, {createFilter} from 'react-search-input'
+
+import ServiceSelect from './ServiceSelect';
+import Loader from './Loader';
+const KEYS_TO_FILTERS = ['name']
+
+const containerStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+};
+
+const imageStyle = {
+  maxWidth: '400px',
+
+}
+
+const innerContainerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  flexGrow: '0',
+};
+
+
+class InformationView extends Component {
+  constructor(props) {
+    super(props);
+    this.state =
+      { serviceName: 'Microsoft Azure',
+        searchTerm: '',
+        artworks: [],
+        loading: true,
+        artwork: null,
+        tagData: null,
+      }
+  }
+  refreshPage(){ 
+      window.location.reload(); 
+  }
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    fetch(`http://localhost:1337/artwork/${id}`)
+      .then(response => response.json())
+      .then(response => {
+        this.setState(
+          { artwork: response,
+            loading: false,
+            tagData: response.tags
+          })
+      })
+      .catch(err => console.error(err.message))
+  }
+
+  searchUpdated (term) {
+    console.log('updating', term)
+    var self = this;
+
+    fetch(`http://localhost:1337/artwork/search/${term}`)
+      .then(response => response.json())
+      .then(function(artworks){
+        self.setState({searchTerm: term, artworks: artworks})
+      })
+      .catch(function(err){
+        console.error('while searching artwork error:', err)
+      })
+  }
+
+  render() {
+    console.log(this.state.artworks)
+    const filteredArtworks = this.state.artworks.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS)).slice(0, 10);
+
+    if (this.state.loading) {
+      return (
+        <Wrapper>
+          <Loader />
+        </Wrapper>
+      )
+    }
+
+    console.log(this.state.artwork)
+
+    return (
+      <Wrapper>
+        <div> Please search for the name of a Painting </div>
+        <SearchInput onChange={this.searchUpdated.bind(this)} fuzzy sortResults/>
+
+        {filteredArtworks.map(artwork => {
+            return (
+              <div className="mail" key={artwork.id}>
+                <Link to={`${artwork.id}`} onClick={ this.refreshPage }>
+                  <div className="from">{artwork.name}</div>
+                </Link>
+              </div>
+            )
+          })}
+        {this.state.artwork ?
+          <div>
+            <Header> {this.state.artwork.name} by {this.state.artwork.artist.name} </Header>
+            <SubHeader> {this.state.artwork.year}</SubHeader>
+            <div style={containerStyle}>
+              <div style={innerContainerStyle}>
+                <div>
+                  <img style={imageStyle} src={`http://localhost:1337/images/${this.state.artwork.url}`} alt="" />
+                </div>
+              </div>
+              <div>
+                <HeaderSmall>
+                  { this.state.serviceName }
+                </HeaderSmall>
+                <ServiceSelect onClick={val => this.setState({ serviceName: val })} />
+                <Table>
+                  <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
+                    <TableRow>
+                      <TableHeaderColumn>Name</TableHeaderColumn>
+                      <TableHeaderColumn>Confidence</TableHeaderColumn>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody displayRowCheckbox={false}>
+                    { this.state.tagData.map((tag, index) => (
+                      <TableRow key={`${tag.name}_${index}`}>
+                        <TableRowColumn><Link to="/information">{tag.name}</Link></TableRowColumn>
+                        <TableRowColumn>{tag.confidence}</TableRowColumn>
+                      </TableRow>
+                      ))}
+                  </TableBody>
+                </Table>
+              </div>
+            </div>
+          </div>
+          : null
+        }
+      </Wrapper>
+    );
+  }
+}
+
+export default InformationView;
--- src/TagView.js
+++ src/TagView.js
@@ -1,54 +1,137 @@
-import React from 'react';
-import SelectField from 'material-ui/SelectField';
-import TextField from 'material-ui/TextField';
-import RaisedButton from 'material-ui/RaisedButton';
-import MenuItem from 'material-ui/MenuItem';
+import React, { Component } from 'react';
+
 import { Link } from 'react-router-dom';
+import Wrapper from './Wrapper';
+import Header from './Header';
+import HeaderSmall from './HeaderSmall';
+import SubHeader from './SubHeader';
 
-const buttonStyle = {
-  marginTop: '20px',
-  marginLeft: '10px',
-};
+import SearchInput, {createFilter} from 'react-search-input'
 
-const outerContainerStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  flexGrow: '0'
-};
+import ServiceSelect from './ServiceSelect';
+import Loader from './Loader';
+const KEYS_TO_FILTERS = ['name']
 
-const containerStyle = {
+const imageContainer = {
   display: 'flex',
   flexDirection: 'row',
+  flexWrap: 'wrap'
+
+}
+
+const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+const containerStyle = {
+  width: '800px'
 };
 
-const Main = () => (
-  <div style={outerContainerStyle}>
-    <div style={containerStyle}>
-      <SelectField
-        floatingLabelText="Service"
-      >
-        <MenuItem value={1} primaryText="IBM Watson" />
-        <MenuItem value={2} primaryText="Tensorflow" />
-        <MenuItem value={3} primaryText="AWS Rekognition" />
-        <MenuItem value={4} primaryText="Google" />
-        <MenuItem value={5} primaryText="Microsoft Azure" />
-        <MenuItem value={6} primaryText="Clarifai" />
-      </SelectField>
-      <TextField
-        floatingLabelText="Search here..."
-      />
-      <Link to="/about">
-        <RaisedButton
-          style={buttonStyle}
-          label="Submit"
-          primary
-        />
-      </Link>
-    </div>
-    <div>
-      <img src="./cezanne.jpg" alt="" />
-    </div>
-  </div>
-);
+const imageStyle = {
+  maxWidth: '200px',
 
-export default Main;
+} 
+
+const innerContainerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  flexGrow: '0',
+};
+
+
+class TagView extends Component {
+  constructor(props) {
+    super(props);
+    this.state =
+      { 
+        searchTerm: '',
+        tags: [],
+        loading: true,
+        artwork: null,
+        tagData: null,
+      }
+  }
+  refreshPage(){ 
+      window.location.reload(); 
+  }
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    console.log(id)
+    fetch(`http://localhost:1337/artwork/query?tag=${id}`)
+      .then(response => response.json())
+      .then(response => {
+        this.setState(
+          {
+            tagName: id,
+            artworks: response,
+            loading: false
+          })
+      })
+      .catch(err => console.error(err.message))
+  }
+
+  searchUpdated (term) {
+    console.log('updating', term)
+    var self = this;
+
+    fetch(`http://localhost:1337/tag/search/${term}`)
+      .then(response => response.json())
+      .then(function(tags){
+        self.setState({searchTerm: term, tags: tags})
+      })
+      .catch(function(err){
+        console.error('while searching artwork error:', err)
+      })
+  }
+
+  render() {
+    console.log(this.state.artworks)
+    const filteredTags = this.state.tags.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS)).slice(0, 10);
+
+    if (this.state.loading) {
+      return (
+        <Wrapper>
+          <Loader />
+        </Wrapper>
+      )
+    }
+
+    console.log(this.state.artwork)
+
+    return (
+      <Wrapper>
+        <div style={containerStyle}>
+          <div> Please search for the name of a Tag </div>
+          <SearchInput onChange={this.searchUpdated.bind(this)} fuzzy sortResults/>
+          <Header> {capitalizeFirstLetter(this.state.tagName)} </Header>
+          
+          {filteredTags.map(tag => {
+              return (
+                <div className="mail" key={tag.id}>
+                  <Link to={`${tag.name}`} onClick={ this.refreshPage }>
+                    <div className="from">{tag.name}</div>
+                  </Link>
+                </div>
+              )
+            })}
+            { this.state.artworks.length > 0 ?
+                <div style={imageContainer}>
+                  {this.state.artworks.map((artwork, index) => {
+                  return (
+                  <div key={`${artwork.id}_${index}`}> 
+                    <Link to={`/artwork/${artwork.id}`}>
+                      <img style={imageStyle} src={`http://localhost:1337/images/${artwork.url}`}/> 
+                    </Link>
+                  </div>)
+                  })}
+                </div>
+                :
+                <div> No images were found for this tag! </div>
+          
+            }
+        </div>
+      </Wrapper>
+    );
+  }
+}
+
+export default TagView;
